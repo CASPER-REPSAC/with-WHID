2b6a327e3f3aacfdf7910436eaf316d9cd4a7a1a
Floodnut <gsniper777@gmail.com>
2021-08-19 05:57:02 +0000

    basic DB

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whatshouldido',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'manager',
+        'USER' : 'django',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
